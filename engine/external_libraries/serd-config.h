337193865dfba7c1cdcf8af074d942a7ae9509dd
Stefan Kersten <stefan@samplecount.com>
2012-03-13 20:04:21 +0100

    Add config.h for lilv, serd and sord

---
+++ engine/external_libraries/serd-config.h
@@ -0,0 +1,8 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef _SERD_CONFIG_H_WAF
+#define _SERD_CONFIG_H_WAF
+
+#define SERD_VERSION "0.5.0"
+
+#endif /* _SERD_CONFIG_H_WAF */
