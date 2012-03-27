61bf7a8687445fef8d60dfa220535c7b39fa47e7
Stefan Kersten <stefan@samplecount.com>
2012-03-28 00:22:47 +0200

    Add interface for sending requests to API

--- engine/platform/jack/Mescaline/API.h
+++ engine/platform/jack/Mescaline/API.h
@@ -1,6 +1,7 @@
 #ifndef Mescaline_API_h
 #define Mescaline_API_h
 
+#include "lv2/lv2plug.in/ns/ext/atom/atom.h"
 #include "lv2/lv2plug.in/ns/ext/urid/urid.h"
 
 #if defined(__cplusplus)
@@ -43,5 +44,14 @@ MESCALINE_EXPORT void Mescaline_Engine_stop(Engine* engine);
 
 MESCALINE_EXPORT LV2_URID Mescaline_Engine_mapUri(Engine* engine, const char* uri);
 MESCALINE_EXPORT const char* Mescaline_Engine_unmapUri(Engine* engine, LV2_URID urid);
+
+typedef void (*MescalineResponseHandler)(const LV2_Atom* response, void* data);
+
+MESCALINE_EXPORT
+void Mescaline_Engine_send(
+	Engine* engine
+  , const LV2_Atom* request
+  , MescalineResponseHandler responseHandler
+  , void* responseHandlerData);
 
 #endif /* Mescaline_API_h */
