9eca3892a4e632a66b1cc212683f2a5f2064214f
Stefan Kersten <stefan@samplecount.com>
2012-03-30 17:27:55 +0200

    Allow to run with library built by release configuration
    Simply define RELEASE=1 in the environment.

--- client/runhaskell.sh
+++ client/runhaskell.sh
@@ -3,4 +3,12 @@
 base=`dirname $0`
 export MESCALINE_LV2_PATH="$base/../engine/lv2/bundles"
 
-runhaskell -L"$base"/../engine/build/debug/macosx/x86_64 -lmescaline "$@"
+if [ -n "$RELEASE" ]; then
+    config=release
+else
+    config=debug
+fi
+
+echo "Using $config configuration ..."
+
+runhaskell -L"$base"/../engine/build/$config/macosx/x86_64 -lmescaline "$@"
