8d50a313d0028f72891fc6d484774560a6a0d2b9
Stefan Kersten <stefan@samplecount.com>
2012-03-31 20:50:35 +0200

    Whitespace fix

--- engine/src/Mescaline/Audio/API.hpp
+++ engine/src/Mescaline/Audio/API.hpp
@@ -9,32 +9,32 @@ namespace Mescaline { namespace Audio { namespace API {
     // typedef boost::function2<void, LV2_Atom*, const LV2_Atom*> HandleResponse;
     typedef void (*HandleResponse)(const LV2_Atom* response, void* data);
 
-	class Request
-	{
-	public:
-		Request(const LV2_Atom* msg, const HandleResponse& handler, void* handlerData)
-			: m_request(msg)
-			, m_handler(handler)
-			, m_handlerData(handlerData)
-		{ }
+    class Request
+    {
+    public:
+        Request(const LV2_Atom* msg, const HandleResponse& handler, void* handlerData)
+            : m_request(msg)
+            , m_handler(handler)
+            , m_handlerData(handlerData)
+        { }
         virtual ~Request()
         { }
 
-		const LV2_Atom* request()
-		{
-		    return m_request;
-		}
+        const LV2_Atom* request()
+        {
+            return m_request;
+        }
 
-	    virtual void respond(const LV2_Atom* msg)
-		{
-		    if (m_handler) m_handler(msg, m_handlerData);
-		}
+        virtual void respond(const LV2_Atom* msg)
+        {
+            if (m_handler) m_handler(msg, m_handlerData);
+        }
 
-	private:
-		const LV2_Atom*	m_request;
+    private:
+        const LV2_Atom* m_request;
         HandleResponse  m_handler;
         void*           m_handlerData;
-	};
+    };
 
 }; }; };
 
