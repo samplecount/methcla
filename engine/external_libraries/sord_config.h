94a203af11765fb143f00c426fb2dbd1c289a04f
Stefan Kersten <stefan@samplecount.com>
2013-01-14 18:20:45 +0100

    Add generated lilv library configuration headers
    These headers are generated by the waf build script on the host (not the target).

---
+++ engine/external_libraries/sord_config.h
@@ -0,0 +1,10 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_SORD_CONFIG_H_WAF
+#define W_SORD_CONFIG_H_WAF
+
+#define HAVE_SERD 1
+#define HAVE_PCRE 1
+#define SORD_VERSION "0.10.4"
+
+#endif /* W_SORD_CONFIG_H_WAF */
