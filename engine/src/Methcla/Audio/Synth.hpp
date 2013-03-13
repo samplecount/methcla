96d61f33fb431894ae5152408b3dd3c20e79d5c1
Stefan Kersten <stefan@samplecount.com>
2013-03-13 12:07:26 +0100

    Whitespace fixes

--- engine/src/Methcla/Audio/Synth.hpp
+++ engine/src/Methcla/Audio/Synth.hpp
@@ -1,11 +1,11 @@
 // Copyright 2012-2013 Samplecount S.L.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,7 @@ namespace Methcla { namespace Audio {
 enum InputConnectionType
 {
     kIn
-  , kInFeedback        
+  , kInFeedback
 };
 
 enum OutputConnectionType
@@ -141,7 +141,6 @@ public:
             }
         }
     }
-
 
 private:
     size_t      m_offset;
