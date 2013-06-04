0c3b5d19076c07c8ddb31585b3659231f4f8e4f1
Stefan Kersten <stefan@samplecount.com>
2013-06-04 15:43:35 +0200

    Convert input SR to millihertz instead of returning an invalid value

--- engine/platform/android/opensl_io.c
+++ engine/platform/android/opensl_io.c
@@ -145,35 +145,9 @@ static void playerCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
 
 static SLuint32 convertSampleRate(SLuint32 sr)
 {
-  switch(sr) {
-  case 8000:
-    return SL_SAMPLINGRATE_8;
-  case 11025:
-    return SL_SAMPLINGRATE_11_025;
-  case 12000:
-    return SL_SAMPLINGRATE_12;
-  case 16000:
-    return SL_SAMPLINGRATE_16;
-  case 22050:
-    return SL_SAMPLINGRATE_22_05;
-  case 24000:
-    return SL_SAMPLINGRATE_24;
-  case 32000:
-    return SL_SAMPLINGRATE_32;
-  case 44100:
-    return SL_SAMPLINGRATE_44_1;
-  case 48000:
-    return SL_SAMPLINGRATE_48;
-  case 64000:
-    return SL_SAMPLINGRATE_64;
-  case 88200:
-    return SL_SAMPLINGRATE_88_2;
-  case 96000:
-    return SL_SAMPLINGRATE_96;
-  case 192000:
-    return SL_SAMPLINGRATE_192;
-  }
-  return -1;
+  const SLuint32 maxSRmHz = 0xFFFFFFFFul;
+  const SLuint32 maxSRHz = maxSRmHz / 1000ul;
+  return sr >= maxSRHz ? maxSRmHz : sr * 10ul;
 }
 
   SLresult result = slCreateEngine(&p->engineObject, 0, NULL, 0, NULL, NULL);
