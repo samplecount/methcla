c7a790776987e33ce5862dfdf7dabe3d5ac2617e
Stefan Kersten <stefan@samplecount.com>
2013-06-06 13:19:29 +0200

    Whitespace fix

--- engine/libs/android/Android.mk
+++ engine/libs/android/Android.mk
@@ -5,4 +5,3 @@ LOCAL_MODULE := methcla
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libmethcla.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../plugins
 include $(PREBUILT_STATIC_LIBRARY)
-
