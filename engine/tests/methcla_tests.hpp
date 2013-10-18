8f6fac9837df340c2bff8bf84580a3af0b05fbee
Stefan Kersten <stefan@samplecount.com>
2013-10-18 13:22:03 +0200

    Add function for approximate floating point equality

--- engine/tests/methcla_tests.hpp
+++ engine/tests/methcla_tests.hpp
@@ -15,11 +15,37 @@
 #ifndef METHCLA_TESTS_HPP_INCLUDED
 #define METHCLA_TESTS_HPP_INCLUDED
 
+#include <cmath>
+#include <limits>
 #include <string>
 
 namespace Methcla { namespace Tests {
     std::string inputFile(const std::string& name);
     std::string outputFile(const std::string& name);
+
+    template <typename T> bool nearlyEqual(T a, T b, T epsilon=std::numeric_limits<T>::epsilon())
+    {
+        const T absA = std::fabs(a);
+        const T absB = std::fabs(b);
+        const T diff = std::fabs(a - b);
+
+        if (a == b)
+        {
+            // Shortcut, handles infinities
+            return true;
+        }
+        else if (a == 0 || b == 0 || diff < std::numeric_limits<T>::denorm_min())
+        {
+            // a or b is zero or both are extremely close to it
+            // relative error is less meaningful here
+            return diff < (epsilon * std::numeric_limits<T>::denorm_min());
+        }
+        else
+        {
+            // use relative error
+            return diff / (absA + absB) < epsilon;
+        }
+    }
 } }
 
 #endif // METHCLA_TESTS_HPP_INCLUDED
