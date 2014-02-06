c7b32d48eae4b7b42228a5170a79d943863765d9
Stefan Kersten <stefan@samplecount.com>
2014-02-06 16:00:25 +0100

    Update sampler example for mac

--- engine/examples/sampler/mac/MethclaSampler/AppDelegate.mm
+++ engine/examples/sampler/mac/MethclaSampler/AppDelegate.mm
@@ -9,34 +9,56 @@
 #import "AppDelegate.h"
 #import "Engine.hpp"
 
+#include <methcla/plugins/soundfile_api_libsndfile.h>
+#include <vector>
+
+using namespace Methcla::Examples::Sampler;
+
+static NSArray * openFiles()
+{
+    NSOpenPanel * panel = [NSOpenPanel openPanel];
+    [panel setAllowsMultipleSelection:YES];
+    [panel setCanChooseDirectories:NO];
+    [panel setCanChooseFiles:YES];
+    [panel setAllowedFileTypes: [NSArray arrayWithObjects:@"wav",@"aiff",nil]];
+    [panel setFloatingPanel:YES];
+    NSInteger result = [panel runModal];
+    return result == NSOKButton ? [panel URLs] : nil;
+}
+
+static std::vector<unichar> keys = {
+    122,97,113,120,115,119,99,100,101,118,102,114,98,103,116,110,104,121,109,106,117,44,107,105,46,108,111,47,59,112
+};
+
+static long keyToIndex(unichar key)
+{
+//    switch (key) {
+//    case 122: return 0;
+//    case 120: return 1;
+//    case  99: return 2;
+//    case 118: return 3;
+//    case  98: return 4;
+//    case  97: return 5;
+//    case 115: return 6;
+//    case 100: return 7;
+//    case 102: return 8;
+//    case 103: return 9;
+//    case 113: return 10;
+//    case 119: return 11;
+//    case 101: return 12;
+//    case 114: return 13;
+//    case 116: return 14;
+//    }
+    auto it = std::find(keys.begin(), keys.end(), key);
+    return it == keys.end() ? -1 : it - keys.begin();
+}
+
 @interface KeyboardView : NSView
 {
     Engine* engine;
 }
 - (void)setEngine:(Engine*)theEngine;
 @end
-
-int keyToIndex(unichar key)
-{
-    switch (key) {
-    case 122: return 0;
-    case 120: return 1;
-    case  99: return 2;
-    case 118: return 3;
-    case  98: return 4;
-    case  97: return 5;
-    case 115: return 6;
-    case 100: return 7;
-    case 102: return 8;
-    case 103: return 9;
-    case 113: return 10;
-    case 119: return 11;
-    case 101: return 12;
-    case 114: return 13;
-    case 116: return 14;
-    }
-    return -1;
-}
 
 @implementation KeyboardView
 - (void)setEngine:(Engine*)theEngine
@@ -53,10 +75,11 @@ int keyToIndex(unichar key)
         NSString* chars = [theEvent charactersIgnoringModifiers];
         if ([chars length] == 1) {
             unichar key = [chars characterAtIndex:0];
-//            NSLog(@"keyDown: %u", key);
-            int index = keyToIndex(key);
+            long index = keyToIndex(key);
+//            NSLog(@"keyDown: key=%u index%d", key, index);
+//            std::cout << key << ",";
             if (index >= 0) {
-                engine->startVoice(static_cast<intptr_t>(index), engine->nextSound(), 0.5);
+                engine->startVoice(static_cast<intptr_t>(index), index, 0.2);
             }
         }
     }
@@ -91,10 +114,23 @@ inline NSString* resourcePath(NSString* component)
 @implementation AppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    Engine::Options options;
+    options.engineOptions.audioDriver.bufferSize = 128;
+    options.engineOptions.addLibrary(methcla_soundfile_api_libsndfile);
+
+    NSArray* sounds = openFiles();
+    options.sounds.reserve([sounds count]);
+    for (id url in sounds)
+    {
+        options.sounds.push_back([[url path] UTF8String]);
+    }
+
     // Set up the sound engine
     try {
         // Initialize and configure the audio engine
-        engine = new Engine([resourcePath(@"sounds") UTF8String]);
+//        options.soundDir = [resourcePath(@"sounds") UTF8String];
+        engine = new Engine(options);
+        engine->useDisk(true);
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
