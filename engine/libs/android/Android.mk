73f135a811f93b34de19d988bc3d7aebc9756f09
Stefan Kersten <stefan@samplecount.com>
2014-02-12 17:24:06 +0100

    Remove unneeded directory from include path

--- engine/libs/android/Android.mk
+++ engine/libs/android/Android.mk
@@ -3,5 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := methcla
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libmethcla.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../plugins
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
