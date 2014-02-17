78dd29c23342592ee08120dfc08e34cde030730d
Stefan Kersten <stefan@samplecount.com>
2014-02-17 18:10:09 +0100

    Update copyright notice

--- engine/include/methcla/platform/rtaudio.hpp
+++ engine/include/methcla/platform/rtaudio.hpp
@@ -1,4 +1,4 @@
-// Copyright 2013 Samplecount S.L.
+// Copyright 2014 Samplecount S.L.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
