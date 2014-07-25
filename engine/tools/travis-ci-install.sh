20ae01af479331e6531dbffa99cdf66195364a89
Stefan Kersten <stefan@samplecount.com>
2014-07-25 15:29:43 +0200

    Set correct CXXFLAGS for building libc++ in the environment

--- engine/tools/travis-ci-install.sh
+++ engine/tools/travis-ci-install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install --allow-unauthenticated -qq clang-3.4
-# export CXXFLAGS="-std=c++0x -stdlib=libc++"
+export CXXFLAGS="-std=c++0x -stdlib=libc++"
 svn co --quiet http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 cd libcxx/lib && bash buildit
 sudo cp ./libc++.so.1.0 /usr/lib/
@@ -8,4 +8,3 @@ cd .. && sudo cp -r include/* /usr/include/c++/v1/
 cd /usr/lib && sudo ln -sf libc++.so.1.0 libc++.so
 sudo ln -sf libc++.so.1.0 libc++.so.1
 cd $cwd
-
